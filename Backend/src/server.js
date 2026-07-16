e906e66d0b496f490e1d08240ec462ee06490d32
Satyam-R4j <satyamraj418@gmail.com>
2026-07-17 02:40:36 +0530

    Worked on routes and reactquery

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,12 +6,17 @@ import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
 import { connectDB } from './lib/db.js'
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT
 
 
 app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173"
+    , credentials : true
+}))
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
